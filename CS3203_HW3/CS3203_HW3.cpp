cc2a128bd86e388b72eee9fb3cf99f9679b48919
Henry-L-Tran <henrytran1129@gmail.com>
2023-02-19 21:44:29 -0600

    Completed a function that computes the sum of a list, and added comments.

--- CS3203_HW3/CS3203_HW3.cpp
+++ CS3203_HW3/CS3203_HW3.cpp
@@ -1,15 +1,21 @@
-// CS3203_HW3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//@author Henry Tran
+
+// CS3203-Assignment3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+//This function takes its parameter and adds a vector containing integers, and returns the sum.
+int adding(vector<int> list) {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    int sum = 0;
+
+    for (int i = 0; i < list.size(); i++) {
+        sum += list.at(i);
+
+    }
+
+    return sum;
+}
